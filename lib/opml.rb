87611a151811823fc3fd1fb87273fbbcba8d84cd
Stephen Benner <sbenner9@gmail.com>
2014-05-27 22:15:29 -0700

    Added `Outline#to_h` (untested)

--- lib/opml.rb
+++ lib/opml.rb
@@ -2,94 +2,102 @@ require 'rexml/document'
 require 'active_support/core_ext/string'
 
 module Opml
-	class Opml
-		class Outline
-			attr_accessor :attributes, :outlines
+  class Opml
+    class Outline
+      attr_accessor :attributes, :outlines
 
-			def initialize(element)
-				@attributes = map_attributes_to_hash(element.attributes)
-				@outlines   = element.elements.collect { |e| Outline.new(e) }
-			end
+      def initialize(element)
+        @attributes = map_attributes_to_hash(element.attributes)
+        @outlines   = element.elements.collect { |e| Outline.new(e) }
+      end
 
-			def flatten
-				@flatten ||= @outlines.map(&:flatten).unshift(self)
-			end
+      def flatten
+        @flatten ||= @outlines.map(&:flatten).unshift(self)
+      end
 
-			def to_s
-				@to_s ||= attributes['text'] || super
-			end
+      def to_s
+        @to_s ||= attributes['text'] || super
+      end
 
-			def respond_to?(method)
-				return true if attributes[method.to_s]
-				super
-			end
+      # todo: TEST THIS!!!
+      def to_hash
+        h = { self.attributes.delete['text'] => self.attributes }
+        unless self.outlines.empty?
+          h.merge self.outlines.to_hash
+        end
+      end
 
-			def method_missing(method, *args)
-				attributes[method.to_s] || super
-			end
+      def respond_to?(method)
+        return true if attributes[method.to_s]
+        super
+      end
 
-			private
+      def method_missing(method, *args)
+        attributes[method.to_s] || super
+      end
 
-			def map_attributes_to_hash(attributes)
-				h = Hash.new
-				attributes.each { |key, value|
-					h[key.underscore] = value
-				}
-				h
-			end
-		end
+      private
 
-		attr_reader :outlines
+      def map_attributes_to_hash(attributes)
+        h = Hash.new
+        attributes.each { |key, value|
+          h[key.underscore] = value
+        }
+        h
+      end
+    end
 
-		def initialize(xml)
-			@doc = REXML::Document.new(xml)
+    attr_reader :outlines
 
-			parse_head_elements :title, :owner_name, :owner_email
-			parse_head_elements :date_created, :date_modified, :with => Proc.new { |e| Time.parse(e) }
+    def initialize(xml)
+      @doc = REXML::Document.new(xml)
 
-			@outlines = document_body ? initialize_outlines_from_document_body : []
-		end
+      parse_head_elements :title, :owner_name, :owner_email
+      parse_head_elements :date_created, :date_modified, :with => Proc.new { |e| Time.parse(e) }
 
-		def flatten
-			@flatten ||= @outlines.map(&:flatten).flatten
-		end
+      @outlines = document_body ? initialize_outlines_from_document_body : []
+    end
 
-		private
+    def flatten
+      @flatten ||= @outlines.map(&:flatten).flatten
+    end
 
-		def parse_head_elements(*elements)
-			options = elements.last.is_a?(Hash) ? elements.pop : {}
-			elements.each do |attribute|
-				define_head_attr_reader(attribute)
-				set_head_value(attribute, options)
-			end
-		end
+    private
 
-		def define_head_attr_reader(attribute)
-			self.class.send(:attr_reader, attribute)
-		end
+    def parse_head_elements(*elements)
+      options = elements.last.is_a?(Hash) ? elements.pop : {}
+      elements.each do |attribute|
+        define_head_attr_reader(attribute)
+        set_head_value(attribute, options)
+      end
+    end
 
-		def get_head_value(attribute)
-			if element = @doc.elements["opml/head/#{attribute.to_s.camelize(:lower)}"]
-				element.text
-			end
-		end
+    def define_head_attr_reader(attribute)
+      self.class.send(:attr_reader, attribute)
+    end
 
-		def parse_value(value, options)
-			options[:with] ? options[:with].call(value) : value
-		end
+    def get_head_value(attribute)
+      if element = @doc.elements["opml/head/#{attribute.to_s.camelize(:lower)}"]
+        element.text
+      end
+    end
 
-		def set_head_value(attribute, options)
-			if value = get_head_value(attribute)
-				instance_variable_set("@#{attribute}", parse_value(value, options))
-			end
-		end
+    def parse_value(value, options)
+      options[:with] ? options[:with].call(value) : value
+    end
 
-		def document_body
-			@document_body ||= @doc.elements['opml/body']
-		end
+    def set_head_value(attribute, options)
+      if value = get_head_value(attribute)
+        instance_variable_set("@#{attribute}", parse_value(value, options))
+      end
+    end
 
-		def initialize_outlines_from_document_body
-			document_body.elements.map { |element| Outline.new(element) }
-		end
-	end
+    def document_body
+      @document_body ||= @doc.elements['opml/body']
+    end
+
+    def initialize_outlines_from_document_body
+      document_body.elements.map { |element| Outline.new(element) }
+    end
+  end
 end
